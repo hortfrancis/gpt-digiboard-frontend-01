164b75ee75d772bee74b2743c8ef9564e415139c
hortfrancis <ajhf@hotmail.co.uk>
2024-04-15 20:31:12 +0100

    Create noticeboard display

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [message, setMessage] = useState('...');
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function updateMessage() {
+        fetch(import.meta.env.VITE_BACKEND_URL + '/message')
+        .then(response => response.json())
+        .then(data => {
+            console.log('data:', data);
+            setMessage(data.message);
+        })
+        .catch(error => console.error(error));
+    }
+
+    useEffect(() => {
+        console.log('VITE_APP_BACKEND_URL:', import.meta.env.VITE_BACKEND_URL);
+        // Fetch the message from the server when the component first mounts, 
+        // then again every 5 seconds
+        updateMessage();
+        setInterval(() => {
+            updateMessage();
+        }, 5000);
+    }, []);
+
+    return (
+        <div className="App">
+            <header>
+                <h1>A digital noticeboard ...</h1>
+            </header>
+            <main>
+                <span className="label">Message:</span>
+                <div className="message-text"><span>{message}</span></div>
+            </main>
+            <footer>
+                <a href="https://github.com/hortfrancis" target="_blank">Alex Hort-Francis 2024</a>
+            </footer>
+
+        </div>
+    )
 }
 
 export default App
